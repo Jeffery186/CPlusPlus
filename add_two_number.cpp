6bf791590ecd1c7cf5bd975890abdfe43dcb27b3
zyhibook <zyh15637540388@gmail.com>
2021-05-07 23:38:32 +0800

    Update Hello_World.cpp

--- add_two_number.cpp
+++ add_two_number.cpp
@@ -4,6 +4,7 @@ int main()
     std::cout << "输入两个数字：" << std::endl;
     int v1 = 0, v2 = 0;
     std::cin >> v1 >> v2;
+//    cin 表示 标准输入；cout 表示 标准输出
     std::cout << "数字 " << v1 << " 和 " << v2
               << " 的和是 " << v1 + v2 << std::endl;
     return 0;
